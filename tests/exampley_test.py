7db1b0aac4fd0858b02b1352c1fee428429ff2e7
Marcell Szikora <szikora.marcell@webness.hu>
2024-07-20 14:50:43 +0200

    test fixes

--- tests/exampley_test.py
+++ tests/exampley_test.py
@@ -1,10 +1,11 @@
 import unittest
 from src.example import add
 
+
 class TestExample(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(1, 2), 3)
-        self.assertEqual(add(10, 10), 20)
+
 
 if __name__ == "__main__":
     unittest.main()
